a7709ad24218fc1f82b1a30b0b72bdae96f7b894
Rahul Devan <inform.rahuldevan@gmail.com>
2025-12-06 17:41:34 +0530

    CORS Issue Fix

--- src/main/java/com/ndash/idsphere/integrations/controller/CheckoutController.java
+++ src/main/java/com/ndash/idsphere/integrations/controller/CheckoutController.java
@@ -16,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/checkout")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class CheckoutController {
 
     private final CheckoutService checkoutService;
